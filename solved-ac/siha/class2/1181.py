b4025560366b6154d526bb29a89ad37af318e57a
sihari-1115 <juliajungwoo@naver.com>
2024-10-30 09:59:43 +0900

    10/28 1181

--- solved-ac/siha/class2/1181.py
+++ solved-ac/siha/class2/1181.py
@@ -1,12 +1,15 @@
 N = int(input()) 
 #리스트 만들고 리스트에 추가 -> 사전 정리 -> 길이 정리
 
-L = [] 
+L = []
 
 for i in range(N):
-    L.append(str(input()))
+    L.append(input().strip())  
+
+L = list(set(L))
 
 L.sort()
-L.sort(key=len) 
+L.sort(key=len)
 
-print(L)
+for word in L:
+    print(word)
