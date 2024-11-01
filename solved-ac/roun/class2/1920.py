4a9edb872aeb029b4a1f226ba191990920fa97db
andantecode <kb721a@naver.com>
2024-11-01 09:05:22 +0900

    roun 1920 modify detail

--- solved-ac/roun/class2/1920.py
+++ solved-ac/roun/class2/1920.py
@@ -6,15 +6,18 @@ n = int(input())
 
 temp = list(map(int, input().split(' ')))
 
+# 빠른 탐색을 위해 dictionary 사용
 answer = {}
 
-for i in range(len(temp)):
-    answer[temp[i]] = 1
+# 각 정수값을 key로 가지는 dictionary로 만듦
+for item in temp:
+    answer[item] = 1
 
 m = int(input())
 
 target = list(map(int, input().split(' ')))
 
+# answer의 key안에 target 값이 있는지 확인해서 출력
 for i in range(len(target)):
     if target[i] in answer.keys():
         print(1)
@@ -22,6 +25,6 @@ for i in range(len(target)):
         print(0)
 
 ###################
-# memory: 51836KB #
-# time:   184KB   #
+# memory: 52992KB #
+# time:   172KB   #
 ###################
