e276f0bb97ad092999d17fe306c8495605ab9252
andantecode <kb721a@naver.com>
2024-11-08 09:05:49 +0900

    roun 10845 11/8

---
+++ solved-ac/roun/class2/10845.py
@@ -0,0 +1,49 @@
+############
+# silver 4 #
+############
+
+from collections import deque
+import sys
+
+def do_command(queue: deque, command: str, value: int = 0):
+
+    if command == 'push':
+        queue.append(value)
+    elif command == 'pop':
+        try:
+            print(queue.popleft())
+        except:
+            print(-1)
+    elif command == 'size':
+        print(len(queue))
+    elif command == 'empty':
+        if len(queue) == 0:
+            print(1)
+        else:
+            print(0)
+    elif command == 'front':
+        try:
+            print(queue[0])
+        except:
+            print(-1)
+    elif command == 'back':
+        try:
+            print(queue[-1])
+        except:
+            print(-1)
+
+
+
+command_num = int(sys.stdin.readline().rstrip())
+
+queue = deque()
+
+for i in range(command_num):
+    command_line = list(sys.stdin.readline().rstrip().split())
+    # command_line을 unboxing해서 넘겨줌
+    do_command(queue, *command_line)
+
+###################
+# memory: 34184KB #
+# time:   72ms    #
+###################
