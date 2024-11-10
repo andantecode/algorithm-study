2a6a42ab96aaeae59ec373294c238963470b380b
sihari-1115 <juliajungwoo@naver.com>
2024-11-11 08:57:44 +0900

    11/11 18110

---
+++ solved-ac/siha/class2/18110.py
@@ -0,0 +1,23 @@
+# 아무 의견 X -> 난이도 0
+# 난이도 의견의 30% 절사평균 (위 15%, 아래 15% 제외하고 평균)
+# 1. 난이도 의견 수와 15% 절사 비율에 따라 제거할 개수 계산
+# 2. 개수에 따라 정렬된 난이도 목록에서 상위 / 하위 의견 제거
+# 3. 남은 의견의 평균 계산 -> 반올림
+# 5 -> 5*0.15 = 0.75(1) -> 0 제외, 4 제외 -> 1~3 평균
+
+import sys
+input = sys.stdin.read
+
+data = input().splitlines()
+n = int(data[0])
+rank = list(map(int, data[1:]))
+
+if n == 0:
+    print(0)
+else:
+    rank.sort()
+    count = round(n*0.15)
+    del_rank = rank[count : n-count]
+    mean = round(sum(del_rank) / len(del_rank)) 
+    print(mean)
+
