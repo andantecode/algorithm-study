22d0c94771cf8953f019eae34b40051e8e84415c
andantecode <kb721a@naver.com>
2024-11-11 09:43:22 +0900

    roun 1929 11/11

--- solved-ac/roun/class2/1929.py
+++ solved-ac/roun/class2/1929.py
@@ -6,14 +6,17 @@ _min, _max = list(map(int, (input().split())))
 output = ''
 
 for i in range(_min, _max+1):
+    count = 0
+
     if i == 1:
         continue
 
     for j in range(2, int(i**0.5)+1):
         if i % j == 0:
+            count = 1
             break
 
-    else:
+    if count == 1:
         output += f'{i}\n'
 
     
