9924671feaf7356a091cf00631d764d03532646d
SEO JIN HYOUNG <tjwlssla1@gmail.com>
2024-11-11 17:00:10 +0900

    fixed 11/11 18110, 1929

--- solved-ac/SEO/class2/1929.py
+++ solved-ac/SEO/class2/1929.py
@@ -1,18 +1,18 @@
 import sys
+import math
 
 M, N = map(int, sys.stdin.readline().split())
 
-if M <= 3:
-    while M < 3:
-        print(M)
-        M += 1
-
 for num in range(M, N+1):
+    if num < 2:
+        continue
     prime = True
 
-    for i in range(2, num):
+    for i in range(2, int(math.sqrt(num))+1):
         if num % i == 0:
             prime = False
             break
     if prime:
         print(num)
+
+# 1은 소수가 아니였다...!뜨든
