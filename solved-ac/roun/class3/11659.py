c66564bf61ba236ad55907614b3ab76098adecf4
andantecode <kb721a@naver.com>
2024-11-27 08:54:49 +0900

    roun 11659 11/27

---
+++ solved-ac/roun/class3/11659.py
@@ -0,0 +1,29 @@
+############
+# silver 3 #
+############
+
+import sys
+
+num, cnt = map(int, sys.stdin.readline().rstrip().split())
+
+# 수
+_list = list(map(int, sys.stdin.readline().rstrip().split()))
+
+dp = [0] * 100001
+
+dp[1] = _list[0]
+
+# 구간 합을 미리 구해놓는다. (이전까지의 합 + 지금의 값)
+for i in range(2, len(_list)+1):
+    dp[i] = dp[i - 1] + _list[i - 1]
+
+# 구간 합[end] - 구간 합[start -1]
+for i in range(cnt):
+    start, end = map(int, sys.stdin.readline().rstrip().split())
+
+    print(dp[end] - dp[start-1])
+
+###################
+# memory: 41116KB #
+# time:   260ms   #
+###################
