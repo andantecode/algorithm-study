e772b18c70928876c460a757f472d41b1bae7cc4
andantecode <kb721a@naver.com>
2024-12-17 09:36:34 +0900

    roun modify file name

--- solved-ac/roun/class3/1931.py
+++ solved-ac/roun/class3/1931.py
@@ -3,43 +3,30 @@
 ############
 
 import sys
-from collections import deque
-
-def bfs(graph: list):
-    visited = [[False for _ in range(len(graph[0]))] for _ in range(len(graph))]
-    visited[0][0] = True
-
-    queue = deque([(0, 0, 1)])
-
-    while queue:
-        row, col, depth = queue.popleft()
-
-        for dy, dx in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-            y, x = row + dy, col + dx
-
-            if 0 <= y < len(graph) and 0 <= x < len(graph[0]):
-                if graph[y][x] and not visited[y][x]:
-                    visited[y][x] = True
-                    queue.append((y, x, depth + 1))
-
-            if (y, x) == (len(graph) - 1, len(graph[0]) - 1):
-                return depth + 1
-    
-    return 0
-
 
 input = sys.stdin.readline
 
-row, col = map(int, input().split())
+num = int(input())
+schedules = []
 
-graph = []
+for i in range(num):
+    schedules.append(list(map(int, input().split())))
 
-for i in range(row):
-    graph.append(list(map(int, input().rstrip())))
+schedules.sort(key=lambda x: (x[1], x[0]))
 
-print(bfs(graph=graph))
+previous_end = 0
+count = 0
+
+for schedule in schedules:
+    start, end = schedule
+
+    if start >= previous_end:
+        count += 1
+        previous_end = end
+    
+print(count)
 
 ###################
-# memory: 35100KB #
-# time:   80ms    #
+# memory: 60360KB #
+# time:   280ms   #
 ###################
