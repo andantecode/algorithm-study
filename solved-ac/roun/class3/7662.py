b3bcf9d6bd894a9578e65165d2d61a93e54c92ee
andantecode <kb721a@naver.com>
2025-01-07 09:03:21 +0900

    roun 7662 1/7

--- solved-ac/roun/class3/7662.py
+++ solved-ac/roun/class3/7662.py
@@ -10,33 +10,44 @@
 import sys
 import heapq
 
+def clean(heap: list, is_max_heap: bool = True):
+    global sync
+
+    while heap:
+        value = -heap[0] if is_max_heap else heap[0]
+        if sync[value] > 0:
+            break
+
+        heapq.heappop(heap)
+
 def do_command(command: str, value: int):
     global min_heap, max_heap, sync
 
     if command == 'I':
         heapq.heappush(min_heap, value)
         heapq.heappush(max_heap, -value)
-        if value in sync:
-            sync[value] += 1
-        else:
-            sync[value] = 1
-    elif command == 'D' and value == 1:
-        if max_heap and min_heap:
-            heapq.heappop(max_heap)
-            min_heap.pop()
-    elif command == 'D' and value == -1:
-        if max_heap and min_heap:
-            heapq.heappop(min_heap)
-            max_heap.pop()
-    
-    print(min_heap)
-    print(max_heap)
+        sync[value] = sync.get(value, 0) + 1
 
+    elif command == 'D' and value == 1:
+        clean(max_heap)
+        if max_heap:
+            max_value = -heapq.heappop(max_heap)
+
+            sync[max_value] -= 1
+
+    elif command == 'D' and value == -1:
+        clean(min_heap, False)
+        if min_heap:
+            min_value = heapq.heappop(min_heap)
+
+            sync[min_value] -= 1
+ 
+    clean(max_heap)
+    clean(min_heap, False)
 
 input = sys.stdin.readline
 
 number_of_cases = int(input())
-
 
 for _ in range(number_of_cases):
     number_of_commands = int(input())
@@ -51,7 +62,17 @@ for _ in range(number_of_cases):
 
         do_command(command, value)
 
-    print(min_heap[-1], min_heap[0]) if min_heap else print("EMPTY")
+    print(-max_heap[0], min_heap[0]) if min_heap else print("EMPTY")
+
+####################
+# memory: 180408KB #
+# time:   6000ms   #
+####################
 
 
+# heap을 2개를 활용 (최대 힙, 최소 힙)
+# sync를 맞춰줄 딕셔너리 하나 추가 -> 각 요소의 개수를 저장해둠
+# 최댓값을 뺀 경우 -> 최대힙에서 pop, sync에서 개수 조정
+# 최솟값을 밴 경우 -> 최소힙에서 pop, sync에서 개수 조정
 
+# 각각 최대, 최소를 빼기 전에, sync 함수를 참조하여 동기화 진행
