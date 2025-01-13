dc6e647ed81ab0b178efc14a21ba0e81278fab67
andantecode <kb721a@naver.com>
2025-01-14 08:54:53 +0900

    roun 11725 1/14

--- solved-ac/roun/class4/11053.py
+++ solved-ac/roun/class4/11053.py
@@ -14,8 +14,9 @@ dp = [0] * number_of_items
 
 dp[0] = 1
 
-for i in range(1, number_of_items):
-    dp[i] = 1
+# 3         
+for i in range(1, number_of_items): 
+    dp[i] = 1 # 그 수를 포함하는 최대 길이를 구하는 거
     for j in range(0, i):
         if items[i] > items[j]:
             dp[i] = max(dp[i], dp[j] + 1)
---
+++ solved-ac/roun/class4/11725.py
@@ -0,0 +1,40 @@
+############
+# silver 2 #
+############
+
+import sys
+from collections import deque
+
+def solve(graph: dict):
+    queue = deque([1])
+    visited = set([1])
+    result = {}
+
+    while queue:
+        curr = queue.popleft()
+
+        for node in graph[curr]:
+            if node not in visited:
+                result[node] = curr
+                queue.append(node)
+                visited.add(node)
+
+    return result
+
+
+
+input = sys.stdin.readline
+
+number_of_nodes = int(input())
+graph = {}
+
+for _ in range(number_of_nodes - 1):
+    node1, node2 = map(int, input().split())
+
+    graph.setdefault(node1, set()).add(node2)
+    graph.setdefault(node2, set()).add(node1)
+
+result = solve(graph)
+
+for key in sorted(result.keys()):
+    print(result[key])
